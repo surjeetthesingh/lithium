8baa2115c09b0755017d780b3967639431618ec5
surjeetthesingh <surjeetthesingh@gmail.com>
2022-10-27 23:02:39 +0530

    session2

---
+++ src/models/bookmodel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName:{
+        type:String,
+        required:true
+    },
+    authorName:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    year:{
+        type:Number,
+        default:2020
+    },
+    tags:{
+        type:[String],
+        required:true
+    },
+    prices:{
+        indianprice:String,
+        europrice:String
+    },
+    totalpages:{
+        type:Number,
+        required:true,
+        default:100,
+    },
+    stockAvailable:{
+        type:Boolean,
+    }
+},{ timestamps:true})
+module.exports = mongoose.model('bookmodel', bookSchema)
