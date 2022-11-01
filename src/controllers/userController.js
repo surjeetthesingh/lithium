0b501d2617bf6422b5acdafcb3f795725130c30c
surjeetthesingh <surjeetthesingh@gmail.com>
2022-11-02 02:01:43 +0530

    jarvis middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,11 @@
 const UserModel= require("../models/userModel")
 
 
-
-
 const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+    console.log( "Assignment is done")
+    res.send({ msg: "!!🕉 हर हर महादेव 🕉!! Your assignment is completes."})
     }
+module.exports.newcode = basicCode
 
 
 
